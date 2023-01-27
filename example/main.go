5755e4261b42cefeaf602705dd2764d3c48f1a20
Aj002Th <2581407059@qq.com>
2023-01-27 20:41:19 +0800

    加入单机缓存服务使用案例

--- example/main.go
+++ example/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"simpleCache"
+)
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func main() {
+	simpleCache.NewGroup("scores", 2<<10, simpleCache.GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	addr := "localhost:9999"
+	peers := simpleCache.NewHttpPool(addr)
+	log.Println("simpelcache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr, peers))
+}
