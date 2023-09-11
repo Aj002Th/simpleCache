631484b41e4242d35951e56b15f22b1a2c3e4130
Aj002Th <2581407059@qq.com>
2023-09-11 22:53:54 +0800

    update: example

--- example/single/run.sh
+++ example/single/run.sh
@@ -1,4 +1,16 @@
-curl http://localhost:9999/_geecache/scores/Tom
-#630
-curl http://localhost:9999/_geecache/scores/kkk
-#not exist
+#!/bin/bash
+trap "rm server;kill 0" EXIT
+
+go build -o server
+./server &
+
+sleep 2
+echo ">>> start test"
+curl "http://localhost:9999/api?key=Tom" #630
+curl "http://localhost:9999/api?key=kkk" #not exist
+
+# old test for peer
+#curl http://localhost:9999/_simplecache/scores/Tom
+#curl http://localhost:9999/_simplecache/scores/kkk
+
+wait
--- example/single/single.go
+++ example/single/single.go
@@ -14,7 +14,7 @@ var dbSingle = map[string]string{
 }
 
 func main() {
-	simpleCache.NewGroup("scores", 2<<10, simpleCache.GetterFunc(
+	sim := simpleCache.NewGroup("scores", 2<<10, simpleCache.GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
 			if v, ok := dbSingle[key]; ok {
@@ -23,8 +23,30 @@ func main() {
 			return nil, fmt.Errorf("%s not exist", key)
 		}))
 
-	addr := "localhost:9999"
+	addr := "http://localhost:8001"
+	apiAddr := "http://localhost:9999"
 	peers := simpleCache.NewHttpPool(addr)
+	peers.Set(addr)
+	sim.RegisterPeerPicker(peers)
+
+	// api server
+	go func() {
+		http.Handle("/api", http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				key := r.URL.Query().Get("key")
+				view, err := sim.Get(key)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				w.Header().Set("Content-Type", "application/octet-stream")
+				_, _ = w.Write(view.ByteSlice())
+			}))
+		log.Println("fontend server is running at", apiAddr)
+		log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+	}()
+
+	// peer server
 	log.Println("simpelcache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
 }
